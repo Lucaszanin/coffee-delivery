ff18b347f0e2781d8dccf9ca83093524f68bf051
Lucas Zanin <lukas.zanin@gmail.com>
2023-10-14 16:06:04 -0300

    add to the images

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -2,6 +2,9 @@ import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
 import { QuickInfoCard } from '../../components/QuickInfoCard/Index'
 import ImageHero from '../../assets/hero-img.svg'
 import * as S from './styles'
+import cafe from '../../assets/americano.png'
+import latte from '../../assets/latte.png'
+import cafecremoso from '../../assets/cremoso.png'
 import { CatalogCard } from '../../components/CatalogCard/Index'
 
 export const Home = () => {
@@ -49,98 +52,98 @@ export const Home = () => {
       <S.MainTitle>Nossos cafés</S.MainTitle>
       <S.MainContainer>
         <CatalogCard
-          productImageUrl="./src/assets/expresso.svg"
+          productImageUrl={cafecremoso}
           coffeType={['Tradicional']}
           productName="Expresso Tradicional"
           description="O tradicional café feito com água quente e grãos moídos"
           productValue={10}
         />
         <CatalogCard
-          productImageUrl="./src/assets/americano.png"
+          productImageUrl={cafe}
           coffeType={['Tradicional']}
           productName="Expresso Americano"
           description="Expresso diluído, menos intenso que o tradicional"
           productValue={10.91}
         />
         <CatalogCard
-          productImageUrl="./src/assets/cremoso.png"
+          productImageUrl={cafecremoso}
           coffeType={['Tradicional']}
           productName="Expresso Cremoso"
           description="Café expresso tradicional com espuma cremosa"
           productValue={11.91}
         />
         <CatalogCard
-          productImageUrl="./src/assets/express-gelado.png"
+          productImageUrl={latte}
           coffeType={['Tradicional', 'Gelado']}
           productName="Expresso Gelado"
           description="Bebida preparada com café expresso e cubos de gelo"
           productValue={9.91}
         />
         <CatalogCard
-          productImageUrl="./src/assets/latte.png"
+          productImageUrl={cafecremoso}
           coffeType={['Tradicional', 'com leite']}
           productName="Latte"
           description="Uma dose de café expresso com o dobro de leite e espuma cremosa"
           productValue={9.91}
         />
         <CatalogCard
-          productImageUrl="./src/assets/expresso.svg"
+          productImageUrl={cafecremoso}
           coffeType={['Tradicional', 'Gelado']}
           productName="Expresso Tradicional"
           description="O tradicional café feito com água quente e grãos moídos"
           productValue={9.91}
         />
         <CatalogCard
-          productImageUrl="./src/assets/expresso.svg"
+          productImageUrl={latte}
           coffeType={['Tradicional', 'Gelado']}
           productName="Expresso Tradicional"
           description="O tradicional café feito com água quente e grãos moídos"
           productValue={9.91}
         />
         <CatalogCard
-          productImageUrl="./src/assets/expresso.svg"
+          productImageUrl={latte}
           coffeType={['Tradicional', 'Gelado']}
           productName="Expresso Tradicional"
           description="O tradicional café feito com água quente e grãos moídos"
           productValue={9.91}
         />{' '}
         <CatalogCard
-          productImageUrl="./src/assets/expresso.svg"
+          productImageUrl={cafecremoso}
           coffeType={['Tradicional', 'Gelado']}
           productName="Expresso Tradicional"
           description="O tradicional café feito com água quente e grãos moídos"
           productValue={9.91}
         />
         <CatalogCard
-          productImageUrl="./src/assets/expresso.svg"
+          productImageUrl={cafe}
           coffeType={['Tradicional', 'Gelado']}
           productName="Expresso Tradicional"
           description="O tradicional café feito com água quente e grãos moídos"
           productValue={9.91}
         />
         <CatalogCard
-          productImageUrl="./src/assets/expresso.svg"
+          productImageUrl={latte}
           coffeType={['Tradicional', 'Gelado']}
           productName="Expresso Tradicional"
           description="O tradicional café feito com água quente e grãos moídos"
           productValue={9.91}
         />
         <CatalogCard
-          productImageUrl="./src/assets/expresso.svg"
+          productImageUrl={cafecremoso}
           coffeType={['Tradicional', 'Gelado']}
           productName="Expresso Tradicional"
           description="O tradicional café feito com água quente e grãos moídos"
           productValue={9.91}
         />{' '}
         <CatalogCard
-          productImageUrl="./src/assets/expresso.svg"
+          productImageUrl={latte}
           coffeType={['Tradicional', 'Gelado']}
           productName="Expresso Tradicional"
           description="O tradicional café feito com água quente e grãos moídos"
           productValue={9.91}
         />
         <CatalogCard
-          productImageUrl="./src/assets/expresso.svg"
+          productImageUrl={cafecremoso}
           coffeType={['Tradicional', 'Gelado']}
           productName="Expresso Tradicional"
           description="O tradicional café feito com água quente e grãos moídos"
