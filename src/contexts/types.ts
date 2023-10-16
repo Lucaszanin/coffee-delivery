b2ef0797f5f790dce1ebdf89e0e1fa00aea8badf
Lucas Zanin <lucas.zanin@grupoboticario.com.br>
2023-10-16 09:26:23 -0300

    add new product in the context

--- src/contexts/types.ts
+++ src/contexts/types.ts
@@ -1,10 +1,22 @@
 import { ReactNode } from 'react'
+import { ProductsDataProps } from '../productsData/types'
+
+export type Product = ProductsDataProps & {
+  quantity: number
+  productId?: string
+}
 
 export type ProductContextProps = {
   total: number
   setTotal: React.Dispatch<React.SetStateAction<number>>
   quantityProduct: number
   setQuantityProduct: React.Dispatch<React.SetStateAction<number>>
+  addProductInCart: (datanew: Product) => void
+  product: Product[]
+}
+
+export type ProductsData = {
+  data: Product[]
 }
 
 export type ProductContextReturn = { children: ReactNode }
